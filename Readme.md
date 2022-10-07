a6e50df68ea009de36aa187c56472ad19101cead
nachoinn <nw_nacho2@hotmail.com>
2022-10-07 18:09:18 -0300

    -Add customtkinter -Fix left and right slide of images bug -Change the spinbox for an entry

---
+++ Readme.md
@@ -0,0 +1,18 @@
+H2O Mark is a UI watermark software based on Tkinter.
+It has basics features to assign single or multiple images to the process of watermarking them.
+You can adjust the watermark size,Opacity, and place it in 5 different positions.
+The software will create a JSON file to make use of the data that needs for the operation.
+It can read or save to this JSON file things like the images that you wish to get watermarked, the watermark will stay
+saved and unchanged after closing the program, the images will get removed of the file if they get watermarked.
+
+Features / Instructions:
+- Required to load watermark before the image/s.
+- Don't delete the data/h2o data.json if you don't want to reset your configs!
+- Set an output folder if you don't want the default one to be used (output images folder will be created).
+- Custom watermark size, opacity, and 5 different positions.
+- Slide through the loaded images.
+- Keep the images saved if they didn't get watermarked yet (get removed if they did).
+- Select an output format(JPEG or PNG)
+- Preview button for use when opacity and positions are changed.
+- For previewing also you can hit enter at the watermark size entry.
+- It looks awesome and it is super simple to use!
--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, filedialog, Canvas, PhotoImage, Label, ttk, Button, IntVar, Radiobutton, Scale, Spinbox,\
+from tkinter import Tk, filedialog, Canvas, PhotoImage, Label, ttk, Button, IntVar, Radiobutton, Scale, Spinbox, \
     StringVar, messagebox, Listbox
 import customtkinter
 from image_processing import ImageProcess
@@ -7,33 +7,37 @@ from PIL import Image, ImageTk
 
 FONT1 = ("Arial", 20, "italic")
 FONT2 = ("Arial", 12, "bold")
-THEME_COLOR = ["#C3F8FF", "#ABD9FF", "#FFF6BF", "#FFEBAD"]
+THEME_COLOR = ["#212325", "#ABD9FF", "#FFF6BF", "#FFEBAD"]
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
 
 
 class AppInterface:
 
     def __init__(self, image_process: ImageProcess, data: DataManager):
-        self.window = Tk()
+        self.window = customtkinter.CTk()
         self.window.geometry("1366x768")
         self.window.title("H2OMark 💧")
-        self.window.config(padx=20, pady=20, bg=THEME_COLOR[0])
+        self.window.config(padx=20, pady=20)
         self.image_process = image_process
         self.data = data
         self.data.save_data({"size": 100})
 
-        self.canvas = Canvas(self.window, height=150, width=120, bg=THEME_COLOR[0], highlightthickness=0)
+        self.canvas = customtkinter.CTkCanvas(self.window, bg=THEME_COLOR[0], height=150, width=120,
+                                              highlightthickness=0)
 
         self.logo = PhotoImage(file="images/H2O-MARK-LOGO.png")
         self.canvas.create_image(60, 80, image=self.logo)
         self.canvas.grid(column=2, row=0, columnspan=3, pady=50)
 
-        self.load_images = Button(text="Load Images ", bd=0, activebackground=THEME_COLOR[1],
-                                  highlightthickness=0, command=self.load_images)
-        self.load_images.grid(column=0, row=1, padx=130, sticky="nw")
+        self.load_images = customtkinter.CTkButton(text="Load Images ", bd=0,
+                                                   highlightthickness=0, command=self.load_images)
+        self.load_images.grid(column=0, row=1, padx=100, sticky="nw")
 
-        self.load_water = Button(text="Load Watermark ", bd=0, activebackground=THEME_COLOR[0],
-                                 highlightthickness=0, command=self.load_watermark)
-        self.load_water.grid(column=0, row=2, padx=130, sticky="nw")
+        self.load_water = customtkinter.CTkButton(text="Load Watermark ", bd=0,
+                                                  highlightthickness=0, command=self.load_watermark)
+        self.load_water.grid(column=0, row=2, padx=100, sticky="nw")
 
         self.watermark_canvas = Canvas(self.window, height=100, width=100, bg=THEME_COLOR[0], highlightthickness=0)
         self.watermark_canvas.grid(column=0, row=3, pady=10, sticky="n")
@@ -57,70 +61,84 @@ class AppInterface:
         except AttributeError:
             pass
 
+        self.left_right_label = customtkinter.CTkLabel(text="You can use Left and Right arrows to slide the images"
+                                                       , text_color="grey").place(x=500, y=620)
+
+        # Position of the watermark
         self.position = IntVar()
-        self.top_left = Radiobutton(text="Top Left Corner", bd=0, activebackground=THEME_COLOR[0], value=1,
-                                    highlightthickness=0, command=self.set_position, variable=self.position)
-        self.top_right = Radiobutton(text="Top Right Corner", bd=0, activebackground=THEME_COLOR[0], value=2,
-                                     highlightthickness=0, command=self.set_position, variable=self.position)
-        self.center = Radiobutton(text="Center", bd=0, activebackground=THEME_COLOR[0], value=3,
-                                  highlightthickness=0, command=self.set_position, variable=self.position)
-        self.bot_left = Radiobutton(text="Bottom Left Corner", bd=0, activebackground=THEME_COLOR[0], value=4,
-                                    highlightthickness=0, command=self.set_position, variable=self.position)
-        self.bot_right = Radiobutton(text="Bottom Right Corner", bd=0, activebackground=THEME_COLOR[0], value=5,
-                                     highlightthickness=0, command=self.set_position, variable=self.position)
+        self.top_left = customtkinter.CTkRadioButton(text="Top Left Corner", value=1, text_color="white",
+                                                     highlightthickness=0, command=self.set_position,
+                                                     variable=self.position)
+        self.top_right = customtkinter.CTkRadioButton(text="Top Right Corner", bd=0, value=2, text_color="white",
+                                                      highlightthickness=0, command=self.set_position,
+                                                      variable=self.position)
+        self.center = customtkinter.CTkRadioButton(text="Center", bd=0, value=3, text_color="white",
+                                                   highlightthickness=0, command=self.set_position,
+                                                   variable=self.position)
+        self.bot_left = customtkinter.CTkRadioButton(text="Bottom Left Corner", bd=0, value=4, text_color="white",
+                                                     highlightthickness=0, command=self.set_position,
+                                                     variable=self.position)
+        self.bot_right = customtkinter.CTkRadioButton(text="Bottom Right Corner", bd=0, value=5, text_color="white",
+                                                      highlightthickness=0, command=self.set_position,
+                                                      variable=self.position)
         self.top_left.grid(column=1, row=4, padx=10, pady=10)
         self.top_right.grid(column=2, row=4, padx=10, pady=10)
         self.center.grid(column=3, row=4, padx=10, pady=10)
         self.bot_left.grid(column=4, row=4, padx=10, pady=10)
         self.bot_right.grid(column=5, row=4, padx=10, pady=10)
 
-        self.spin_label = Label(text="Watermark Size Percentage").place(x=130, y=550)
+        self.spin_label = customtkinter.CTkLabel(master=self.window, text="Watermark Size Percentage",
+                                                 text_color="white").place(x=100, y=545)
 
-        self.spinvar = IntVar(self.window)
-        self.spinvar.set(100)
-        self.size_spin = Spinbox(from_=0, to=1000, increment=1, textvariable=self.spinvar, command=self.size)
-        self.size_spin.grid(column=0, row=3, padx=130, pady=20, sticky="s")
+        self.intvar = IntVar(self.window)
+        self.intvar.set(100)
+        self.size_spin = customtkinter.CTkEntry(placeholder_text=100, textvariable=self.intvar,
+                                                validatecommand=self.size)
+        self.size_spin.grid(column=0, row=3, padx=100, pady=20, sticky="s")
         self.size_spin.bind('<Return>', self.size)
 
-        self.opacity = Scale(from_=100, to=0, command=self.set_opacity, label="Opacity")
-        self.opacity.grid(column=0, row=4, padx=130, pady=10)
+        self.opacity = customtkinter.CTkSlider(from_=0, to=100, command=self.set_opacity,
+                                               orient="vertical", height=110)
+        self.opacity.grid(column=0, row=4, padx=100, sticky="n")
 
-        self.preview_process = Button(text="Preview ", bd=0, activebackground=THEME_COLOR[0],
-                                      highlightthickness=0, command=self.preview_image)
+        self.preview_process = customtkinter.CTkButton(text="Preview ", bd=0,
+                                                       highlightthickness=0, command=self.preview_image)
         self.preview_process.grid(column=6, row=1, padx=20, pady=20)
 
-        self.insert_water = Button(text="Watermark the Images  ", bd=0, activebackground=THEME_COLOR[0],
-                                   highlightthickness=0, command=self.insert_water_button)
+        self.insert_water = customtkinter.CTkButton(text="Watermark the Images  ", bd=0,
+                                                    highlightthickness=0, command=self.insert_water_button)
         self.insert_water.grid(column=6, row=2, padx=20, pady=20)
 
         self.window.bind('<Left>', self.select_image_left)
         self.window.bind('<Right>', self.select_image_right)
 
-        self.output_folder = Button(text="Output Folder:", command=self.output_folder)
+        self.output_folder = customtkinter.CTkButton(text="Output Folder:", command=self.output_folder)
         self.output_folder.grid(column=6, row=3, padx=20, pady=20)
-        self.out_label = Label(text="..." + self.data.reading_data("out_folder")[-45:])
-        self.out_label.place(x=1070, y=560)
+        self.out_label = customtkinter.CTkLabel(text="..." + self.data.reading_data("out_folder")[-45:],
+                                                text_color="white")
+        self.out_label.place(x=1040, y=560)
 
-        self.format_ = Listbox(height=2)
-        self.format_.insert(0, "jpeg")
-        self.format_.insert(1, "png")
-        self.format_.bind("<<ListboxSelect>>", self.output_format)
+        self.format_ = customtkinter.CTkOptionMenu(values=["JPEG", "PNG"], command=self.output_format)
         self.format_.grid(column=6, row=4, padx=20, pady=20)
-        self.format_label = Label(text="Output Images Format:")
-        self.format_label.place(x=1045, y=640)
+        self.format_label = customtkinter.CTkLabel(text="Output Images Format:", text_color="white")
+        self.format_label.place(x=1050, y=630)
 
         self.window.mainloop()
 
     def load_images(self):
         """Load images to watermark into a list and saves it into .json file."""
-
-        files_path = filedialog.askopenfiles(title="Select your images to watermark")
-        images = self.image_process.process_images(files_path)
-        print(images)
-        if not images:
-            messagebox.showerror(title="Error", message="You must select at least one image to insert a watermark to.")
+        if not self.data.reading_data("watermark"):
+            messagebox.showerror(title="Error",
+                                 message="Load your watermark first please.")
         else:
-            self.preview_image()
+            files_path = filedialog.askopenfiles(title="Select your images to watermark")
+            images = self.image_process.process_images(files_path)
+            print(images)
+            if not images:
+                messagebox.showerror(title="Error",
+                                     message="You must select at least one image to insert a watermark to.")
+            else:
+                self.preview_image()
 
     def load_watermark(self):
         """Loads watermark and returns a path location string."""
@@ -143,6 +161,8 @@ class AppInterface:
             return file_path.name
 
     def insert_water_button(self):
+        if not self.data.reading_data("out_folder"):
+            messagebox.showerror(title="Error", message="Output folder is not set, please put an destination.")
         watermark_path = self.data.reading_data("watermark")
         if not watermark_path:
             watermark_path = self.load_watermark()
@@ -171,66 +191,80 @@ class AppInterface:
         self.watermark_canvas.grid(column=0, row=3, pady=10, sticky="n")
         watermark = ImageTk.PhotoImage(Image.open(self.data.reading_data("watermark")).resize((100, 100)))
         self.watermark_canvas.create_image(50, 50, image=watermark)
-        self.spin_label = Label(text="Watermark Size Percentage").place(x=130, y=550)
+        self.spin_label = customtkinter.CTkLabel(text="Watermark Size Percentage",
+                                                 text_color="white").place(x=100, y=550)
         self.watermark_canvas.mainloop()
 
     def preview_image(self, first=0):
         first = self.data.reading_data("select")
-        if not self.data.reading_data("watermark"):
-            messagebox.showerror(title="Error",
-                                 message="Watermark not loaded.")
-        else:
-            self.image_canvas = Canvas(self.window, height=330, width=450, bg=THEME_COLOR[0], highlightthickness=0)
-            self.image_canvas.grid(column=1, row=1, columnspan=5, rowspan=3, pady=20, padx=20, sticky="n")
-            image = self.image_process.insert_watermark(self.data.reading_data("watermark"), preview_mode=True)
-            if type(image) == list:
-                if len(image) > first >= 0:
-                    image = image[first]
-                    if image:
-                        pil_image = image.resize((450, 330))
-                        image = ImageTk.PhotoImage(pil_image)
-                        self.image_canvas.create_image(225, 165, image=image)
-                        self.image_canvas.mainloop()
-                else:
-                    self.data.save_data({"select": 0})
+        self.image_canvas = Canvas(self.window, height=330, width=450, bg=THEME_COLOR[0], highlightthickness=0)
+        self.image_canvas.grid(column=1, row=1, columnspan=5, rowspan=3, pady=20, padx=20, sticky="n")
+        image = self.image_process.insert_watermark(self.data.reading_data("watermark"), preview_mode=True)
+        if type(image) == list:
+            if len(image) > first >= 0:
+                image = image[first]
+                if image:
+                    pil_image = image.resize((450, 330))
+                    image = ImageTk.PhotoImage(pil_image)
+                    self.image_canvas.create_image(225, 165, image=image)
+                    self.image_canvas.mainloop()
+            else:
+                self.data.save_data({"select": 0})
 
     def size(self, event=None):
         try:
-            size = int(self.size_spin.get())
+            size_ = int(self.size_spin.get())
         except ValueError:
             messagebox.showerror(title="Error",
                                  message="Please input round numbers only.")
         else:
-            if size > 1000 or size <= 0:
+            if size_ > 1000 or size_ < 0:
                 messagebox.showerror(title="Error",
                                      message="Please input a size lower than 1000% and higher than 0%")
             size_dict = {
-                "size": size
+                "size": size_
             }
             self.data.save_data(size_dict)
             if event:
                 self.preview_image()
 
     def select_image_left(self, event=None):
-        first = self.data.reading_data("select") -1
-        select_dict = {
-            "select": first
-        }
-        self.data.save_data(select_dict)
-        self.preview_image(self.data.reading_data("select"))
+        first = self.data.reading_data("select")
+        images = len(self.data.reading_data("images"))
+        if images == 1:
+            pass
+        elif first == 0:
+            pass
+        else:
+            select_dict = {
+                "select": first - 1
+            }
+            self.data.save_data(select_dict)
+            self.preview_image(self.data.reading_data("select"))
 
     def select_image_right(self, event=None):
         first = self.data.reading_data("select")
-        self.data.save_data({"select": first+1})
-        selection = self.data.reading_data("select")
-        self.preview_image(selection)
+        images = len(self.data.reading_data("images"))
+        if images == 1:
+            pass
+        elif first == (images - 1):
+            pass
+        else:
+            self.data.save_data({"select": first + 1})
+            selection = self.data.reading_data("select")
+            self.preview_image(selection)
 
     def output_folder(self):
         output_path = filedialog.askdirectory(title="Select your Output Folder")
-        self.data.save_data({"out_folder": output_path})
-        self.out_label = Label(text="..." + self.data.reading_data("out_folder")[-45:])
-        self.out_label.place(x=1070, y=560)
+        if not output_path:
+            pass
+        else:
+            self.data.save_data({"out_folder": output_path})
+            self.out_label.destroy()
+            self.out_label = customtkinter.CTkLabel(text="..." + self.data.reading_data("out_folder")[-45:],
+                                                    text_color="white")
+            self.out_label.place(x=1040, y=560)
 
     def output_format(self, event):
-        format_selected = self.format_.get(self.format_.curselection())
+        format_selected = self.format_.get()
         self.data.save_data({"format": format_selected})
